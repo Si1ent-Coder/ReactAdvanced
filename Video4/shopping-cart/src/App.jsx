308e05f2d0c808de35dfcfad6613c11f322864db
Raghav Chauhan <1raghav.chauhan@gmail.com>
2026-08-01 18:07:44 +0530

    Updates dev

---
+++ Video4/shopping-cart/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./Components/Counter";
+
+const App=()=>{
+  return(
+    <div>
+      <Counter></Counter>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ Video4/shopping-cart/src/Components/Counter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../Redux/Slices/cartSlice";
+
+
+const Counter=()=>{ 
+
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+
+    return(
+        <div>
+            <button onClick={()=> dispatch(increment())}>
+                Increment
+            </button> 
+            <br />
+            <br />
+
+
+            <div>{count}</div>
+            
+            <button onClick={()=> dispatch(decrement())}>
+                Decrement
+            </button>
+
+
+        </div>
+    )
+}
+
+export default Counter;
